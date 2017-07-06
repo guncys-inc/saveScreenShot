dbc92f25e13048bf886d258f9c9a225213c48b03
Tetsuya Nozawa <t.nozawa@guncys.com>
2017-07-06 14:29:40 +0900

    updated README

--- README.md
+++ README.md
@@ -5,6 +5,7 @@
 <MODULEPATH> module path
 
 # max run
+
 imgpath = @"<IMAGEPATH>"
 pythoncode = stringStream ""
 format "import sys;sys.path.append(r'<MODULEPATH>');from saveScreenShot.exts import max as ssmax;ssmax.SaveScreenShot(r'%')" imgpath to:pythoncode
@@ -12,12 +13,17 @@ python.Execute pythoncode
 
 
 # maya run
+
 from saveScreenShot.exts import maya as ssmaya
 ssmaya.SaveScreenShot(r"<IMAGEPATH>")
 
 
 # motionbuilder run
-# output image must be in
-#     ".bmp", ".pbm", ".pgm", ".png", ".ppm", ".xbm", ".xpm" 
+
+```
+	output image must be in
+    ".bmp", ".pbm", ".pgm", ".png", ".ppm", ".xbm", ".xpm" 
+```
+
 from saveScreenShot.exts import mobu as ssmobu
 ssmobu.SaveScreenShot(r"<IMAGEPATH>")
---
+++ exts/mobu/__init__.py
@@ -0,0 +1,57 @@
+from ... import util
+from ... import cropImage
+from ... import tiff2bitmap
+
+
+import pyfbsdk as fb
+
+import os
+
+
+def _saveBuffer(savePath, tmpImgExtension="tif"):
+
+    app = fb.FBApplication()
+    take = fb.FBSystem().CurrentTake
+    current = fb.FBTime(fb.FBPlayerControl().GetEditCurrentTime())
+    next = fb.FBTime(fb.FBPlayerControl().GetEditCurrentTime() + 1)
+
+    opts = fb.FBVideoGrabber().GetOptions()
+    videoManager = fb.FBVideoCodecManager()
+    videoManager.VideoCodecMode = fb.FBVideoCodecMode.FBVideoCodecUncompressed
+
+    opts.OutputFileName = savePath
+    opts.RenderAudio = False
+    opts.BitsPerPixel = fb.FBVideoRenderDepth.FBVideoRender32Bits
+    opts.TimeSpan = fb.FBTimeSpan(current, next)
+    opts.TimeStep = fb.FBTime(0, 0, 0, 1, 0)
+
+    app.FileRender( opts )
+
+
+def SaveScreenShot(outPath, tmpImgExtension="tif"):
+    util.CheckDirectory(outPath)
+    tmp_file = util.GetTempImgPath(extension=tmpImgExtension)
+
+    _saveBuffer(tmp_file)
+    current = fb.FBTime(fb.FBPlayerControl().GetEditCurrentTime())
+    tmp_file = "{0}{1:04d}.tif".format(tmp_file.split(".")[0], current.GetFrame())
+    tmp_bmp = tmp_file.replace(".tif", ".bmp")
+    tiff2bitmap.execute(tmp_file, tmp_bmp)
+
+
+    result = cropImage.CropImage.RunCropImage(tmp_bmp, outPath, parent=None)
+
+    os.remove(tmp_file)
+    os.remove(tmp_bmp)
+
+    return (result is 1)
+
+
+def GetMayaMainWindow():
+    from ...Qt import QtWidgets
+
+    parent = next(
+        o for o in QtWidgets.QApplication.instance().topLevelWidgets()
+        if "MotionBuilder " in o.windowTitle()
+    )
+    return parent
